df003afa0483d09e15f9393b1faf8b7513f4d44b
Frank <wsgoodman@163.com>
2019-06-05 18:19:10 +0800

    result 页面的数据还差两个表格没有拼接。

--- app/pods/page-result/route.js
+++ app/pods/page-result/route.js
@@ -1,6 +1,16 @@
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
 
 export default Route.extend({
+	model() {
+		const indexModel = this.modelFor('index'),
+			{ detailProposal, scenario } = indexModel;
+
+		return hash({
+			scenario,
+			detailProposal
+		});
+	},
 	afterModel() {
 		let applicationController = this.controllerFor('application');
 
--- app/pods/product-sales-report/model.js
+++ app/pods/product-sales-report/model.js
@@ -6,5 +6,12 @@ export default DS.Model.extend({
 	salesQuota: DS.attr('number'),	// 销售指标
 	share: DS.attr('number'),	// 份额
 	quotaAchievement: DS.attr('number'),	// 指标达成率
+	potential: DS.attr('number'),	//	潜力
+	quotaContribute: DS.attr('number'),	// 指标贡献率
+	quotaGrowth: DS.attr('number'),		// 指标增长率
+	salesContribute: DS.attr('number'),	// 销售额贡献率
+	salesMonthOnMonth: DS.attr('number'),// 销售额环比增长
+	salesYearOnYear: DS.attr('number'),		// 销售额同比增长
+	ytdSales: DS.attr('number'),	// YTD 销售额
 	goodsConfig: DS.belongsTo()
 });
